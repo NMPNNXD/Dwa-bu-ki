4f3592938bfae32fe0505b653f1bfa2d5ea67b3c
NMPNNXD <148040146+NMPNNXD@users.noreply.github.com>
2025-10-08 12:57:01 +0200

    Server.js

---
+++ Server.js
@@ -0,0 +1,67 @@
+let http = require('http');
+const fs = require('fs');
+const url = require('url');
+let data;
+let data1;
+let counter = 0;
+fs.readFile("plik.txt", "utf-8",(err,data)=>{
+  if(err){
+    console.error('error reading file:',err);
+    return;
+  }else{
+     counter = parseInt(data);
+    
+  }
+  
+})
+
+http.createServer(function (req, res) {
+counter++;
+  var x = 0;
+  const parsedUrl = url.parse(req.url,true);
+  const path = parsedUrl.pathname;
+ if(path === "/" ){ 
+  res.writeHead(200, {'Content-Type': 'text/html'});res.end('Witaj na stronie.odwiedziles ja juz '+counter.toString()+ ' razy');}
+  else if(path === "/add"){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+  const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1;
+const day = currentDate.getDate();
+const now = new Date();
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
+
+fs.appendFile("guests.txt", "Jan; "+ hours+ " - " + minutes + " - " + seconds + " "+ day +","+ month +","+ year  + "\n", (err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+       
+    }
+});
+ res.end('Dodano Jana');   
+  }
+   else if(path === "/list"){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile("guests.txt", "utf-8",(err,data1)=>{
+  if(err){
+    console.error('error reading file:',err);
+    return;
+  }else{
+   
+   res.end(data1);
+    
+  }
+  
+})
+
+    
+  }
+  
+  fs.writeFileSync('plik.txt',counter.toString(),'utf8');
+  console.log('Zapisano dane do pliku.');
+
+})
+.listen(3000);
